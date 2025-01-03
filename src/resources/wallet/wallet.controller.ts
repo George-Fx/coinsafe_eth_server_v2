6b96151bf414812ab1204a3c3b8d082f87c2fae4
George <kul.giorgi@gmail.com>
2025-01-03 15:50:17 +0400

    update

--- src/resources/wallet/wallet.controller.ts
+++ src/resources/wallet/wallet.controller.ts
@@ -1,11 +1,11 @@
-import {Controller, All} from '@nestjs/common';
+import {Controller, Get} from '@nestjs/common';
 import {WalletService} from './wallet.service';
 
 @Controller('wallet')
 export class WalletController {
   constructor(private readonly walletService: WalletService) {}
 
-  @All('create')
+  @Get('create')
   createWallet() {
     return this.walletService.createWallet();
   }
